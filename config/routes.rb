95428ff5255d7927699ecffbc13acb1a29496b3e
David Liu <davidbliu@gmail.com>
2016-02-02 00:24:29 -0800

    .

--- config/routes.rb
+++ config/routes.rb
@@ -41,6 +41,8 @@ Rails.application.routes.draw do
   get 'push' => 'push#index'
   get 'push/register'  => 'push#register'
 
+  # points
+  get 'points' => 'points#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
