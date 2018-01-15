911de39b0061c686465b62c4ee070fadb25fb6ab
Ubuntu <daliu@daliu-vm1.ixcpaj3afazutnwd3kpbkl3veg.bx.internal.cloudapp.net>
2018-01-15 22:58:07 +0000

    Modify deploy.sh and haproxy.cfg for azure

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 fuser -k 3000/tcp
 fuser -k 3001/tcp
 fuser -k 3002/tcp
-nohup rails s -p 3000 -e production -P tmp/pids/server1.pid &
-nohup rails s -p 3001 -e production -P tmp/pids/server2.pid &
-nohup rails s -p 3002 -e production -P tmp/pids/server3.pid &
+nohup rails s -p 3000 -e production -P -b 0.0.0.0 tmp/pids/server1.pid &
+nohup rails s -p 3001 -e production -P -b 0.0.0.0 tmp/pids/server2.pid &
+nohup rails s -p 3002 -e production -P -b 0.0.0.0 tmp/pids/server3.pid &
